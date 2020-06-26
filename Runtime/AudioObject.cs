b1eaf457810830597e1319bffbb7fbd6aef5d861
aftah <abdelfetah.hamra@skynet.be>
2020-06-26 14:28:03 +0200

    Initial Commit

---
+++ Runtime/AudioObject.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace BulletHell.Audio
+{
+    public enum TypeOfAudio
+
+    {
+
+        NONE,
+        SOUND_TRACK,
+        SFX,
+        SFX_SHOOT,
+        SFX_RELOAD,
+        SFX_EXPLOSION,
+        SFX_IMPACT
+
+    }
+
+   
+
+    [System.Serializable]
+    public class AudioObject
+    {
+        [Header("Audio Object")]
+        public TypeOfAudio TypeOfAudio;
+        public AudioSource audioSource;
+        public AudioMixerGroup outpup;
+
+        [Header("Audio Clip")]
+        public string name;
+        public AudioClip clip;
+
+        [Header("Clip Sound Effect")]
+        [Range(0,1)]
+        public float volume = 1f;
+
+        [Range(0,3)]
+        public float pitch = 1f;
+
+        [Range(-1,1)]
+        public float stereoPan = 0f;
+
+        public bool loop = false;
+        public bool mute = false;
+        public bool playOnAwake = false;
+
+
+
+        public void Play()
+        {
+
+            audioSource.volume = volume;
+            audioSource.pitch = pitch;
+            audioSource.panStereo = stereoPan;
+            audioSource.loop = loop;
+            audioSource.mute = mute;
+            audioSource.outputAudioMixerGroup = outpup;
+
+            audioSource.Play();
+
+        }
+
+        public void Stop()
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+
+        }
+    }
+
+
+}
+
---
+++ Runtime/SoundController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BulletHell.Helper;
+
+
+namespace BulletHell.Audio
+{
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SoundController : Singleton<SoundController>
+    {
+        [SerializeField]
+        private AudioObject[] audioObject;
+
+        private void Start()
+        {
+
+            for (int i = 0; i < audioObject.Length; i++)
+            {
+                if (audioObject[i].audioSource == null && audioObject[i].clip != null)
+                {
+                    GameObject gameObject = new GameObject("Clip_" + i + "_" + audioObject[i].name);
+                    gameObject.transform.SetParent(this.transform);
+                    audioObject[i].audioSource = gameObject.AddComponent<AudioSource>();
+                    audioObject[i].audioSource.clip = audioObject[i].clip;
+
+
+                }
+
+                if (audioObject[i].playOnAwake)
+                {
+                    audioObject[i].Play();
+                    return;
+                }
+
+            }
+        }
+
+
+        public void PlaySound(string name)
+        {
+
+            for (int i = 0; i < audioObject.Length; i++)
+            {
+                if (audioObject[i].name == name)
+                {
+                    audioObject[i].Play();
+                    return;
+                }
+
+            }
+            Debug.LogWarning("Sound Manager(Play) : Cannot find the clip name in the list !!!");
+
+        }
+
+        public void StopSound(string name)
+        {
+
+
+            for (int i = 0; i < audioObject.Length; i++)
+            {
+                if (audioObject[i].name == name)
+                {
+                    audioObject[i].Stop();
+                    return;
+                }
+
+            }
+            Debug.LogWarning("Sound Manager(Stop) : Cannot find the clip name in the list !!!");
+
+
+
+        }
+
+
+
+
+    }
+}
